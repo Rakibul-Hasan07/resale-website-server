85bbbea25168922bf7e2928131817c162bcd10b2
shihab00027 <shihabhasan981@gmail.com>
2022-11-28 12:50:50 +0600

    get id wise category product

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-require('dotenv').config();
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors')
+require('dotenv').config();
+
 
 //middle ware
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('resale server are running')
+})
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9wccxu2.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 async function run() {
     try {
@@ -39,45 +43,25 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/users/:email', async (req, res) => {
-            const email = req.params.email;
-            const query = { email: email };
-            const result = await usersCollection.findOne(query);
-            res.send(result);
-        })
-        app.get('/users/admin/:email', async (req, res) => {
-            const email = req.params.email;
-            const query = { email: email }
-            const user = await usersCollection.findOne(query)
-            res.send({ isAdmin: user?.role === 'admin' })
-        })
-        app.get('/users/buyer/:email', async (req, res) => {
-            const email = req.params.email;
-            const query = { email: email }
-            const user = await usersCollection.findOne(query)
-            res.send({ isBuyer: user?.role === 'buyer' })
-        })
-        app.get('/users/seller/:email', async (req, res) => {
-            const email = req.params.email;
-            const query = { email: email }
-            const user = await usersCollection.findOne(query)
-            res.send({ isSeller: user?.role === 'seller' })
-        })
-        app.get('/users', async (req, res) => {
-            const query = {}
-            const user = await usersCollection.find(query).toArray();
-            res.send(user)
-        })
+       
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             const result = await bookingsCollection.insertOne(booking)
             res.send(result)
         })
+        app.get('/bookings', async (req, res) => {
+            const getEmail = req.query.email;
+            const query = { email: getEmail }
+            const result = await bookingsCollection.find(query).toArray();
+            res.send(result);
+        })
+
         app.get('/blogs', async (req, res) => {
             const query = {};
             const result = await blogsCollection.find(query).toArray();
             res.send(result)
         })
+        
     }
     catch (error) {
         console.log(error);
@@ -85,9 +69,7 @@ async function run() {
 }
 run().catch(console.dir)
 
-app.get('/', (req, res) => {
-    res.send('resale server are running')
-})
+
 
 app.listen(port, () => {
     console.log(`server are running on: ${port}`);
